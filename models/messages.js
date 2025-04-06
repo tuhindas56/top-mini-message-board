3205461ceb025dff2598b253ea11e7968913358b
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-04-06 08:09:57 +0000

    feat: add message model and getMessages function

---
+++ models/messages.js
@@ -0,0 +1,18 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+]
+
+function getMessages() {
+  return [...messages]
+}
+
+export { getMessages }
