7605a0bfa0db2a7244ff255949611e968dfed509
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-04-14 16:26:44 +0000

    refactor: add fallback for when there are no messages

--- controller/home.js
+++ controller/home.js
@@ -2,7 +2,6 @@ const { getMessages } = require("../models/messages.js")
 
 async function homeController(req, res) {
   const messages = await getMessages()
-  res.render("index", { messages })
+  res.render("index", { messages: !messages.length ? undefined : messages })
 }
-
 module.exports = homeController
