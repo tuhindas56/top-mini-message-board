eb0fb2ea110be6c4d09b2c813b6c86361745cfc8
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-04-15 06:02:04 +0000

    chore: remove redundant code

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 8080
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded())
 
 app.get("/", homeController)
